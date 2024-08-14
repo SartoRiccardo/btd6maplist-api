7481142d69ac9d06e53c55193575ddcc8a95c0d6
SartoRiccardo <rikki.sarto@gmail.com>
2024-08-14 17:22:57 +0200

    fix: maps/leaderboard supports version query parameter

--- api/maps/leaderboard/route.py
+++ api/maps/leaderboard/route.py
@@ -3,5 +3,18 @@ from src.db.queries.leaderboard import get_maplist_leaderboard
 
 
 async def get(request: web.Request):
-    lb = await get_maplist_leaderboard()
+    current_version = True
+    if "version" in request.query:
+        version = request.query["version"].lower()
+        if version.lower() == "all":
+            current_version = False
+        elif version != "current":
+            return web.json_response(
+                {
+                    "error": 'Allowed values for "ver": ["current", "all"]'
+                },
+                status=400,
+            )
+
+    lb = await get_maplist_leaderboard(curver=current_version)
     return web.json_response([entry.to_dict() for entry in lb])
