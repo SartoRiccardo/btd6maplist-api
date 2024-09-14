359dbdb4ef544b1b4e0dc2003bf13ed31eda840a
SartoRiccardo <rikki.sarto@gmail.com>
2024-09-14 14:12:29 +0200

    fix: PartialUser as argument

--- src/db/queries/leaderboard.py
+++ src/db/queries/leaderboard.py
@@ -14,6 +14,7 @@ async def get_maplist_leaderboard(idx_start=0, amount=50, curver=True, conn=None
         FROM {lb_view} lb
         JOIN users u
             ON u.discord_id = lb.user_id
+        ORDER BY lb.placement
         LIMIT $1
         OFFSET $2
         """,
@@ -32,8 +33,22 @@ async def get_maplist_leaderboard(idx_start=0, amount=50, curver=True, conn=None
 @postgres
 async def get_maplist_lcc_leaderboard(curver=True, conn=None):
     lb_view = "list_curver_lcclb" if curver else "list_allver_lcclb"
-    payload = await conn.fetch(f"SELECT user_id, score, placement FROM {lb_view}")
+    payload = await conn.fetch(
+        f"""
+        SELECT
+            lb.score, lb.placement,
+            u.discord_id, u.name
+        FROM {lb_view} lb
+        JOIN users u
+            ON u.discord_id = lb.user_id
+        ORDER BY lb.placement
+        """
+    )
     return [
-        LeaderboardEntry(row[0], float(row[1]), row[2])
+        LeaderboardEntry(
+            PartialUser(row[2], row[3], None, False),
+            float(row[0]),
+            row[1]
+        )
         for row in payload
     ]
