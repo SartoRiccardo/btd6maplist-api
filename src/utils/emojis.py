796de5c0ae35bf08e103ee021aadd726aee2153b
SartoRiccardo <rikki.sarto@gmail.com>
2024-09-14 12:39:53 +0200

    feat: Added emojis

--- src/utils/emojis.py
+++ src/utils/emojis.py
@@ -1,11 +1,11 @@
 
 
 class Emj:
-    no_geraldo = ""
-    lcc = ""
-    black_border = ""
-    gold_border = ""
+    no_geraldo = "<:m_noopthero:1284093053321089116>"
+    lcc = "<:m_lcc:1284093037391253526>"
+    black_border = "<:m_bb:1284092992294092810>"
+    gold_border = "<:m_win:1284093064247246921>"
 
-    curver = ""
-    allver = ""
-    experts = ""
+    curver = "<:i_curver:1284134344860106775>"
+    allver = "<:i_allver:1284134312664633374>"
+    experts = "<:i_hard:1284134361310036018>"
