daa65c2b698150d124f7c6c908cb3f0b9be84468
SartoRiccardo <rikki.sarto@gmail.com>
2024-09-18 00:21:52 +0200

    fix: Update emojis

--- src/utils/emojis.py
+++ src/utils/emojis.py
@@ -1,11 +1,11 @@
 
 
 class Emj:
-    no_geraldo = "<:m_noopthero:1284093053321089116>"
-    lcc = "<:m_lcc:1284093037391253526>"
-    black_border = "<:m_bb:1284092992294092810>"
-    gold_border = "<:m_win:1284093064247246921>"
+    no_geraldo = "<:m_noopthero:1285726689845968936>"
+    lcc = "<:m_lcc:1285726686117236828>"
+    black_border = "<:m_bb:1285726683709702205>"
+    gold_border = "<:m_win:1285726692110893079>"
 
-    curver = "<:i_curver:1284134344860106775>"
-    allver = "<:i_allver:1284134312664633374>"
-    experts = "<:i_hard:1284134361310036018>"
+    curver = "<:i_curver:1285727183477674037>"
+    allver = "<:i_allver:1285726693922967634>"
+    experts = "<:i_hard:1285726697496514560>"
