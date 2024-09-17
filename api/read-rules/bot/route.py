723661c538e1cef402e244ab4b31ee858e996f0d
SartoRiccardo <rikki.sarto@gmail.com>
2024-09-17 13:08:23 +0200

    feat: Added bot route for PUT /read-rules

---
+++ api/read-rules/bot/route.py
@@ -0,0 +1,14 @@
+from aiohttp import web
+import http
+import src.utils.routedecos
+from src.db.queries.users import read_rules
+
+
+@src.utils.routedecos.check_bot_signature()
+async def put(
+        _r: web.Request,
+        json_data: dict = None,
+        **_kwargs
+) -> web.Response:
+    await read_rules(int(json_data["id"]))
+    return web.Response(status=http.HTTPStatus.OK)
