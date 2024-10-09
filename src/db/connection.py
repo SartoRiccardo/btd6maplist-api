983c6c9785684d7e5d2b2f61801870ce451a8b42
SartoRiccardo <rikki.sarto@gmail.com>
2024-10-09 14:58:46 +0200

    feat: Can apply patches

--- src/db/connection.py
+++ src/db/connection.py
@@ -1,3 +1,5 @@
+import os.path
+
 import asyncpg
 import config
 from src.utils.colors import red, purple
@@ -14,6 +16,7 @@ async def start():
             database=config.DB_NAME, host=config.DB_HOST
         )
         print(f"{purple('[PSQL]')} Connected")
+        await update_schema()
         await init_database()
     except:
         print(f"{purple('[PSQL]')} {red('Error connecting to Postgres database')}")
@@ -35,9 +38,39 @@ def postgres(wrapped):
 
 @postgres
 async def init_database(conn=None):
-    with open("database/views.psql") as fviews:
+    with open(os.path.join("database", "views.psql")) as fviews:
         await conn.execute(fviews.read())
-        print(f"{purple('[PSQL]')} Created views")
-    with open("database/triggers.psql") as ftriggers:
+        print(f"{purple('[PSQL/Init]')} Created views")
+    with open(os.path.join("database", "triggers.psql")) as ftriggers:
         await conn.execute(ftriggers.read())
-        print(f"{purple('[PSQL]')} Created triggers")
+        print(f"{purple('[PSQL/Init]')} Created triggers")
+
+
+@postgres
+async def update_schema(conn=None):
+    dbinfo_path = os.path.join(config.PERSISTENT_DATA_PATH, "dbinfo.txt")
+    patches_path = os.path.join("database", "patches")
+    if not os.path.exists(dbinfo_path):
+        with open(dbinfo_path, "w") as fdb:
+            fdb.write("0")
+
+    with open(dbinfo_path) as fdb:
+        last_update = int(fdb.read())
+
+    print(f"{purple('[PSQL/Schema]')} Last update: {last_update}")
+    updates = sorted(os.listdir(patches_path))
+    for upd in updates:
+        upd_day, _ = upd.split("-", 1)
+        upd_day = int(upd_day)
+        if upd_day <= last_update:
+            continue
+        patch_path = os.path.join(patches_path, upd)
+        print(f"{purple('[PSQL/Schema]')} Applying {patch_path}")
+        with open(patch_path) as fpatch:
+            await conn.execute(f"BEGIN; {fpatch.read()} COMMIT;")
+
+        last_update = upd_day
+
+    with open(dbinfo_path, "w") as fdb:
+        fdb.write(str(last_update))
+
