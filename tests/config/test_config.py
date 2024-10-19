a9ff73b3afd175d7487af55ffaf9e85cef4fcd91
SartoRiccardo <rikki.sarto@gmail.com>
2024-10-19 13:22:45 +0200

    fix: Fixture name fix

--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -2,13 +2,13 @@ import pytest
 
 
 @pytest.mark.get
-async def test_get_config(btd6ml_app):
+async def test_get_config(btd6ml_test_client):
     """Test getting config vars"""
     pytest.skip("Not Implemented")
 
 
 @pytest.mark.put
-async def test_edit_config(btd6ml_app):
+async def test_edit_config(btd6ml_test_client):
     """
     Test editing config variables, and that some variables are correctly
     gated by their required perms (ML Mod, Expert Mod, both or Admin-only)
