8489f0325a50659ef38769afa7dd529563fff44d
SartoRiccardo <rikki.sarto@gmail.com>
2024-10-22 13:25:12 +0200

    tests: LCC leaderboard tests

--- tests/leaderboard/test_leaderboard.py
+++ tests/leaderboard/test_leaderboard.py
@@ -77,6 +77,32 @@ class TestLeaderboard:
                 points += config[config_keys[comp["map"]["difficulty"]]]
         return points
 
+    @staticmethod
+    async def calc_lcc_count(
+            user_id: int,
+            formats: list[int],
+            btd6ml_test_client,
+            req_field: str = "placement_cur",
+    ) -> int:
+        async with btd6ml_test_client.get(
+                f"/users/{user_id}/completions?formats={','.join(str(x) for x in formats)}"
+        ) as resp:
+            user_comps = sorted(
+                (await resp.json())["completions"],
+                key=lambda x: (x["map"]["code"], x["current_lcc"]),
+                reverse=True,
+            )
+        lcc_count = 0
+        for i, comp in enumerate(user_comps):
+            if req_field == "placement_cur" and comp["map"]["placement_cur"] not in range(1, 50):
+                continue
+            elif req_field == "difficulty" and comp["map"]["difficulty"] == -1:
+                continue
+
+            if comp["current_lcc"] and (i == 0 or user_comps[i-1]["map"]["code"] != comp["map"]["code"]):
+                lcc_count += 1
+        return lcc_count
+
     async def test_ml_point_leaderboard(self, btd6ml_test_client):
         """Test the leaderboard is correctly calculated"""
         USER_ID = 47
@@ -107,11 +133,31 @@ class TestLeaderboard:
 
     async def test_ml_lcc_leaderboard(self, btd6ml_test_client):
         """Test the maplist lcc leaderboard is correctly calculated"""
-        pytest.skip("Not Implemented")
+        USER_ID = 39
+        lcc_count = await self.calc_lcc_count(USER_ID, [1], btd6ml_test_client)
+
+        async with btd6ml_test_client.get("/maps/leaderboard?format=1&value=lccs") as resp:
+            assert resp.status == http.HTTPStatus.OK, \
+                f"Getting the leaderboard returns {resp.status}"
+            usr_entry = next(
+                entry for entry in (await resp.json())["entries"]
+                if entry["user"]["id"] == str(USER_ID)
+            )
+            assert usr_entry["score"] == lcc_count, "User LCC count differs from expected"
 
     async def test_exp_lcc_leaderboard(self, btd6ml_test_client):
         """Test the expert lcc leaderboard is correctly calculated"""
-        pytest.skip("Not Implemented")
+        USER_ID = 46
+        lcc_count = await self.calc_lcc_count(USER_ID, [1, 51], btd6ml_test_client, req_field="difficulty")
+
+        async with btd6ml_test_client.get("/maps/leaderboard?format=51&value=lccs") as resp:
+            assert resp.status == http.HTTPStatus.OK, \
+                f"Getting the leaderboard returns {resp.status}"
+            usr_entry = next(
+                entry for entry in (await resp.json())["entries"]
+                if entry["user"]["id"] == str(USER_ID)
+            )
+            assert usr_entry["score"] == lcc_count, "User LCC count differs from expected"
 
 
 @pytest.mark.completions
