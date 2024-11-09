e26c09575c1636fee188fc379df633c2939ad244
SartoRiccardo <rikki.sarto@gmail.com>
2024-11-10 00:15:45 +0100

    fix: Read rules tests

--- tests/users/test_read_rules.py
+++ tests/users/test_read_rules.py
@@ -11,21 +11,21 @@ async def test_read_rules(btd6ml_test_client, mock_auth):
     USERNAME = "test_new_usr"
     await mock_auth(user_id=USER_ID, username=USERNAME)
 
-    async with btd6ml_test_client.post("/auth?discord_token=test_token") as resp:
+    async with btd6ml_test_client.post("/auth", headers=HEADERS) as resp:
         assert resp.status == http.HTTPStatus.OK, \
             f"Authenticating as a new user returns {resp.status}"
         resp_data = await resp.json()
-        assert not resp_data["maplist_profile"]["has_seen_popup"], "New user has seen popup"
+        assert not resp_data["has_seen_popup"], "New user has seen popup"
 
     async with btd6ml_test_client.put("/read-rules", headers=HEADERS) as resp:
         assert resp.status == http.HTTPStatus.NO_CONTENT, f"Reading the rules returns {resp.status}"
 
-    async with btd6ml_test_client.post("/auth?discord_token=test_token") as resp:
+    async with btd6ml_test_client.post("/auth", headers=HEADERS) as resp:
         resp_data = await resp.json()
-        assert resp_data["maplist_profile"]["has_seen_popup"], "New user has still not seen popup"
+        assert resp_data["has_seen_popup"], "New user has still not seen popup"
 
     async with btd6ml_test_client.put("/read-rules", headers=HEADERS) as resp:
         assert resp.status == http.HTTPStatus.NO_CONTENT, f"Reading the rules returns {resp.status}"
 
-    async with btd6ml_test_client.post("/auth?discord_token=test_token") as resp:
+    async with btd6ml_test_client.post("/auth", headers=HEADERS) as resp:
         assert resp_data == await resp.json(), "Profile changed when reading the rules twice"
