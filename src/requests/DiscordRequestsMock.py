32968135d59b2b26a4dcf2454aca3eb75c9a7987
SartoRiccardo <rikki.sarto@gmail.com>
2025-02-24 15:40:53 +0100

    feat: Mocked new Discord endpoints

--- src/requests/DiscordRequestsMock.py
+++ src/requests/DiscordRequestsMock.py
@@ -4,6 +4,9 @@ import re
 import aiohttp
 import random
 import config
+from .DiscordModelsMock import RoleMock, MemberMock, GuildMock
+
+roles = [RoleMock(i, f"Role{i}", i) for i in range(15)]
 
 
 def get_token_meta(access_token: str) -> tuple[int, int]:
@@ -22,6 +25,23 @@ class DiscordRequestsMock(DiscordRequests):
     ADMIN = 2 ** 6
     MAPLIST_OWNER = 2 ** 7
     EXPLIST_OWNER = 2 ** 8
+
+    _bot_guilds = [
+        GuildMock(1, "G1", roles=roles, member=MemberMock(roles=roles[8:10])),
+        GuildMock(2, "G2", roles=roles, member=MemberMock(roles=roles[8:10])),
+        GuildMock(3, "G3", roles=roles, member=MemberMock(roles=roles[8:10]), permissions=P_MANAGE_ROLES),
+        GuildMock(4, "G4", roles=roles, member=MemberMock(roles=roles[8:10]), permissions=P_ADMIN),
+        GuildMock(5, "G5", roles=roles, member=MemberMock(roles=roles[13:]), permissions=P_ADMIN | P_MANAGE_ROLES),
+        GuildMock(7, "G7", roles=roles, member=MemberMock(roles=roles[8:10]), permissions=P_MANAGE_ROLES),
+    ]
+    _user_guilds = [
+        GuildMock(1, "G1", roles=roles, member=MemberMock(roles=roles[8:10])),
+        GuildMock(2, "G2", roles=roles, member=MemberMock(roles=roles[8:10]), permissions=P_ADMIN),
+        GuildMock(3, "G3", roles=roles, member=MemberMock(roles=roles[8:10])),
+        GuildMock(4, "G4", roles=roles, member=MemberMock(roles=roles[13:]), permissions=P_MANAGE_ROLES),
+        GuildMock(5, "G5", roles=roles, member=MemberMock(roles=roles[8:10]), permissions=P_ADMIN | P_MANAGE_ROLES),
+        GuildMock(6, "G6", roles=roles, member=MemberMock(roles=roles[8:10]), permissions=P_MANAGE_ROLES),
+    ]
 
     @staticmethod
     async def get_user_profile(access_token: str) -> dict:
@@ -97,7 +117,6 @@ class DiscordRequestsMock(DiscordRequests):
             # "bio": "",
         }
 
-
     @staticmethod
     async def execute_webhook(hook_url: str, data: aiohttp.FormData, wait: bool = False) -> str:
         if wait:
@@ -110,3 +129,28 @@ class DiscordRequestsMock(DiscordRequests):
     @staticmethod
     async def delete_webhook(hook_url: str, message_id: str) -> None:
         pass
+
+    @staticmethod
+    async def get_user_guilds(self, *args, **kwargs) -> list[dict]:
+        return [g.to_dict() for g in DiscordRequestsMock._user_guilds]
+
+    @staticmethod
+    async def get_application_guilds(*args, **kwargs) -> list[dict]:
+        return [g.to_dict() for g in DiscordRequestsMock._bot_guilds]
+
+    @staticmethod
+    async def get_guild_roles(guild_id: str | int) -> list[dict]:
+        if isinstance(guild_id, str):
+            guild_id = int(guild_id)
+        for guild in DiscordRequestsMock._bot_guilds:
+            if guild_id == guild.id:
+                return [r.to_dict() for r in guild.roles]
+
+    @staticmethod
+    async def get_application_guild_member(guild_id: str | int) -> dict | None:
+        if isinstance(guild_id, str):
+            guild_id = int(guild_id)
+        try:
+            return next(g for g in DiscordRequestsMock._bot_guilds if g.id == guild_id).member.to_dict()
+        except StopIteration:
+            return None
