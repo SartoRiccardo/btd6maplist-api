5a09624444f4e8dcba925ba5107be87e4b4020f4
SartoRiccardo <rikki.sarto@gmail.com>
2025-03-30 19:22:23 +0200

    docs: Docstring fix

--- src/db/models/MapSubmission.py
+++ src/db/models/MapSubmission.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime
+import src.utils.formats
 
 
 @dataclass
@@ -18,9 +19,15 @@ class MapSubmission:
         description: Additional notes provided in the submission.
       format:
         $ref: "#/components/schemas/MaplistFormat"
-      proposed_difficulty:
+      proposed_diff:
         type: string
         description: The proposed difficulty
+      proposed_diff_name:
+        type: string
+        nullable: true
+        description: |
+          The proposed difficulty as a string. Nullable if it's a dynamic value
+          such as Nostalgia Pack remake maps.
       rejected_by:
         $ref: "#/components/schemas/DiscordID"
         nullable: true
@@ -42,12 +49,18 @@ class MapSubmission:
     wh_data: str | None
 
     def to_dict(self) -> dict:
+        diff_names = src.utils.formats.format_idxs[self.format_id].proposed_values
+        diff_name = None
+        if isinstance(diff_names, tuple) and 0 <= self.proposed_diff < len(diff_names[1]):
+            diff_name = diff_names[1][self.proposed_diff]
+
         return {
             "code": self.code,
             "submitter": str(self.submitter),
             "subm_notes": self.subm_notes,
             "format": self.format_id,
             "proposed_diff": self.proposed_diff,
+            "proposed_diff_name": diff_name,
             "rejected_by": str(self.rejected_by) if self.rejected_by else None,
             "completion_proof": self.completion_proof,
             "created_on": int(self.created_on.timestamp()),
