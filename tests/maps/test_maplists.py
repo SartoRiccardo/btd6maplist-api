e7854a0d7940e5fe2072dd8557d1e76774133be9
SartoRiccardo <rikki.sarto@gmail.com>
2025-04-06 11:39:43 +0200

    feat: Tested maplists and retro maps

--- tests/maps/test_maplists.py
+++ tests/maps/test_maplists.py
@@ -13,7 +13,24 @@ list_map_schema = {
 @pytest.mark.maps
 @pytest.mark.get
 class TestMaplists:
+    async def test_retro_maps(self, btd6ml_test_client):
+        """Test the retro maps endpoint"""
+        retro_map_schema = {"id": int, "name": str}
+
+        async with btd6ml_test_client.get("/maps/retro") as resp:
+            assert resp.ok, f"GET /maps returned {resp.status}"
+            resp_data = await resp.json()
+            for game_name in resp_data:
+                for diff_name in resp_data[game_name]:
+                    seen_ids = set()
+                    for i, retro_map in enumerate(resp_data[game_name][diff_name]):
+                        assert retro_map["id"] not in seen_ids, f"Encountered id {retro_map['id']} twice"
+                        assert len(src.utils.validators.check_fields(retro_map, retro_map_schema)) == 0, \
+                            f"Error while validating RetroMap[{i}]"
+                        seen_ids.add(retro_map["id"])
+
     async def test_maplist(self, btd6ml_test_client):
+        """Test the Current Version maplist"""
         async with btd6ml_test_client.get("/maps") as resp:
             assert resp.ok, f"GET /maps returned {resp.status}"
             resp_data = await resp.json()
@@ -27,6 +44,7 @@ class TestMaplists:
                     f"Error while validating ListMap[{i}]"
 
     async def test_maplist_allvers(self, btd6ml_test_client):
+        """Test the All Versions maplist"""
         async with btd6ml_test_client.get("/maps?format=2") as resp:
             assert resp.ok, f"GET /maps returned {resp.status}"
             resp_data = await resp.json()
@@ -39,14 +57,38 @@ class TestMaplists:
                 assert len(src.utils.validators.check_fields(map_data, list_map_schema)) == 0, \
                     f"Error while validating ListMap[{i}]"
 
-    async def test_expert_list(self, btd6ml_test_client):
+    async def test_expert_lists(self, btd6ml_test_client):
+        """Test the Expert List and Best of the Best packs"""
+        for format_id in [51, 52]:
+            async with btd6ml_test_client.get(f"/maps?format={format_id}&filter=1") as resp:
+                assert resp.ok, f"GET /maps returned {resp.status}"
+                resp_data = await resp.json()
+                for i, map_data in enumerate(resp_data):
+                    assert len(src.utils.validators.check_fields(map_data, list_map_schema)) == 0, \
+                        f"Error while validating Map[{i}] ({format_id=})"
 
-        async with btd6ml_test_client.get("/maps?format=51") as resp:
+    async def test_nostalgia_pack(self, btd6ml_test_client):
+        """Test the nostalgia pack"""
+        np_schema = {
+            **list_map_schema,
+            "code": str | None,
+            "format_idx": {
+                "id": int,
+                "name": str,
+                "sort_order": int,
+                "preview_url": str,
+                "game": {"id": int, "name": str},
+                "category": {"id": int, "name": str},
+                "subcategory": {"id": int, "name": str | None},
+            },
+        }
+
+        async with btd6ml_test_client.get(f"/maps?format=11&filter=4") as resp:
             assert resp.ok, f"GET /maps returned {resp.status}"
             resp_data = await resp.json()
             for i, map_data in enumerate(resp_data):
-                assert len(src.utils.validators.check_fields(map_data, list_map_schema)) == 0, \
-                    f"Error while validating ExpertMap[{i}]"
+                assert len(src.utils.validators.check_fields(map_data, np_schema)) == 0, \
+                    f"Error while validating NostalgiaPackMap[{i}]"
 
     async def test_legacy_list(self, btd6ml_test_client):
         """Test the legacy list and its pagination"""
